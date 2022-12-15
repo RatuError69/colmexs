14dad610651737c13ef6fba9b4d59833431acada
JESSICA PUTRY <120353642+RatuError69@users.noreply.github.com>
2022-12-15 18:48:01 +0700

    Update run.py

--- run.py
+++ run.py
@@ -170,13 +170,13 @@ def menu():
 
 def activate_licensi():
 	os.system("clear")
-	print("\n\n\x1b[1;97mSudah mempunyai licensi key? ketik \x1b[1;92madmin\x1b[1;97m untuk chat admin, ketik \x1b[1;94mgets\x1b[1;97m untuk mengambil licensi melalui website RATUERROR\n")
+	print("\n\n\x1b[1;97mSudah mempunyai licensi key? ketik \x1b[1;92madmin\x1b[1;97m untuk chat admin, ketik \x1b[1;93mgets\x1b[1;97m untuk mengambil licensi melalui website RATUERROR\n")
 	key = input(" [>] licensi: ").lower()
 	if "gets" in key:
 		os.system("xdg-open https://fbkey.ratuerror.com/register/")
 		activate_licensi()
 	elif "admin" in key:
-		os.system("xdg-open https://wa.me/6287799183568?text=Jessica%20cantik....beli%20lisensi%20dooong")
+		os.system("xdg-open https://wa.me/6287799183568?text=RATU%20COLMEX's....beli%20lisensi%20dooong")
 		activate_licensi()
 	else:
 		gets = requests.get("https://fbkey.ratuerror.com/check.php?key=%s&dev=%s" % (key.strip(), platform.platform())).json()
